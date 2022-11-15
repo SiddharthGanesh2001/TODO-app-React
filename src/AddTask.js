a9b78fda889f02c7aa285026b40d3af4f10c07e1
SiddharthGanesh2001 <siddharthganesh2001@gmail.com>
2022-11-15 18:52:37 +0530

    changed methods

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,9 +1,19 @@
-export default function AddTask() {
-    
+import { TaskCollection } from "./App";
+import TaskBox from "./TaskBox";
+export default function AddTask({ onSubmit }) {
+
+  function handleEnter(event) {
+    let taskName = document.querySelector("#addTaskBox").value;
+    if(event.code === 'Enter') {
+      onSubmit(taskName);
+      TaskBox();
+    }
+  }
+
   return(
     <div id="addTask">    
-        <input type="text" name="addTask" placeholder="Add a task" id="addTaskBox" autoComplete="off"/>
-        <input type="button" value="Add" id="add" onClick={this.addHandler}/>    
+        <input type="text" name="addTask" placeholder="Add a task" id="addTaskBox" autoComplete="off" onKeyDown={handleEnter}/>
+        <br/><br/>
     </div>
   )
 }
